52941176522b54cb1bb3ae4f3eab78f1f42b61d5
Anna Becker and Isabelle George <anna.r.becker@gmail.com>
2016-02-09 15:26:02 -0800

    add tests to output string for numbers up to 99

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,16 +10,78 @@ public class App {
 
   public static String numberWord(Integer number) {
     String results = "";
-    HashMap<Integer, String> under20Map = new HashMap<Integer, String>();
-    under20Map.put(1, "one");
-    under20Map.put(2, "two");
-    under20Map.put(3, "three");
+    HashMap<Integer, String> under10Map = new HashMap<Integer, String>();
+    under10Map.put(1, "one");
+    under10Map.put(2, "two");
+    under10Map.put(3, "three");
+    under10Map.put(4, "four");
+    under10Map.put(5, "five");
+    under10Map.put(6, "six");
+    under10Map.put(7, "seven");
+    under10Map.put(8, "eight");
+    under10Map.put(9, "nine");
 
-    for (Object key : under20Map.keySet()){
-     if (key == number){
-       results = under20Map.get(key);
+    HashMap<Integer, String> under20Map = new HashMap<Integer, String>();
+    under20Map.put(10, "ten");
+    under20Map.put(11, "eleven");
+    under20Map.put(12, "twelve");
+    under20Map.put(13, "thirteen");
+    under20Map.put(14, "fourteen");
+    under20Map.put(15, "fifteen");
+    under20Map.put(16, "sixteen");
+    under20Map.put(17, "seventeen");
+    under20Map.put(18, "eighteen");
+    under20Map.put(19, "nineteen");
+
+    HashMap<Integer, String> tensMap = new HashMap<Integer, String>();
+    tensMap.put(20, "twenty");
+    tensMap.put(30, "thirty");
+    tensMap.put(40, "forty");
+    tensMap.put(50, "fifty");
+    tensMap.put(60, "sixty");
+    tensMap.put(70, "seventy");
+    tensMap.put(80, "eighty");
+    tensMap.put(90, "ninety");
+
+    if (number < 10){
+      for (Object key : under10Map.keySet()){
+       if (key == number){
+         results = under10Map.get(key);
+        }
       }
     }
+
+    if (number > 10 && number < 20){
+      for (Object key : under20Map.keySet()){
+       if (key == number){
+         results = under20Map.get(key);
+        }
+      }
+    }
+
+    if (number % 10 == 0){
+      for (Object key : tensMap.keySet()){
+        if (key == number){
+          results = tensMap.get(key);
+        }
+      }
+    }
+    if (number > 20){
+      for (Integer key : tensMap.keySet()){
+        if (key / 10 == number / 10){
+          results = tensMap.get(key);
+        }
+      }
+      for (Integer key : under10Map.keySet()){
+        if (key % 10 == number % 10){
+          results = results.concat(" " + under10Map.get(key));
+        }
+      }
+    }
+    //
+    // if (number > 20 && number % 10 > 0){
+    //
+    // }
     return results;
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,10 +4,30 @@ import static org.junit.Assert.*;
 public class AppTest {
 
   @Test
-   public void numberWord_translateANumberLessThan20ToAWord_word() {
+   public void numberWord_translateANumberLessThan10ToAWord_word() {
      App numbersToWords = new App();
      String word = "two";
      assertEquals(word, numbersToWords.numberWord(2));
    }
 
+   @Test
+    public void numberWord_translateANumberLessThan20ToAWord_word() {
+      App numbersToWords = new App();
+      String word = "twelve";
+      assertEquals(word, numbersToWords.numberWord(12));
+    }
+
+   @Test
+    public void numberWord_translateANumberByTens_word() {
+      App numbersToWords = new App();
+      String word = "twenty";
+      assertEquals(word, numbersToWords.numberWord(20));
+    }
+
+   @Test
+    public void numberWord_translateANumberBetweenTens_word() {
+      App numbersToWords = new App();
+      String word = "twenty two";
+      assertEquals(word, numbersToWords.numberWord(22));
+    }
 }
