4b49516a1ddc77dd5e654c28e6e56f5ea8df2bf5
Anna Becker and Isabelle George <10izzygeorge@gmail.com>
2016-02-09 13:33:21 -0800

    pass test to output one if 1 is entered

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,16 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) {}
 
+  public static String numberWord(Integer number) {
+    String outPut = "";
+    String[] lessThan20 = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    for (Integer i= 0; i <= lessThan20.length; i++) {
+      if (i == number) {
+       outPut = lessThan20[i-1];
+      }
+    }
+    return outPut;
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,4 +2,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AppTest {
+
+  @Test
+   public void numberWord_translateANumberToAWord_word() {
+     App numbersToWords = new App();
+     String word = "one";
+     assertEquals(word, numbersToWords.numberWord(1));
+   }
 }
